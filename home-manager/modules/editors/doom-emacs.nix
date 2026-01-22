24b0b1055cc9efcccf5d8c148639f1533e6439fd
rahulbabu7 <rahulbabu436@gmail.com>
2026-01-22 16:59:59 +0530

    Update doom-emacs.nix

--- home-manager/modules/editors/doom-emacs.nix
+++ home-manager/modules/editors/doom-emacs.nix
@@ -1,19 +1,33 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   programs.emacs = {
     enable = true;
     package = pkgs.emacs30-pgtk;
-    extraPackages = epkgs: with epkgs; [
-      vterm
-      pdf-tools
-      # xclip
-    ];
+    extraPackages =
+      epkgs: with epkgs; [
+        vterm
+        pdf-tools
+        # xclip
+      ];
   };
 
   home.packages = with pkgs; [
-    git ripgrep fd 
-    (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
+    git
+    ripgrep
+    fd
+    (aspellWithDicts (
+      dicts: with dicts; [
+        en
+        en-computers
+        en-science
+      ]
+    ))
     nil
     pyright
     nodePackages.typescript-language-server
@@ -30,16 +44,21 @@
     nodePackages.js-beautify
   ];
 
-  home.activation.installDoomEmacs = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    if [ ! -d "$HOME/.emacs.d" ]; then
+  home.activation.installDoomEmacs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -x "$HOME/.emacs.d/bin/doom" ]; then
       echo "Installing Doom Emacs..."
-      $DRY_RUN_CMD ${pkgs.git}/bin/git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.emacs.d
-      $DRY_RUN_CMD $HOME/.emacs.d/bin/doom install --no-env --no-fonts
+      rm -rf "$HOME/.emacs.d"
+
+      ${pkgs.git}/bin/git clone --depth 1 \
+        https://github.com/doomemacs/doomemacs \
+        "$HOME/.emacs.d"
+
+      EMACS=${pkgs.emacs30-pgtk}/bin/emacs \
+        "$HOME/.emacs.d/bin/doom" install --no-env --no-fonts
     fi
   '';
 
   # Link to your existing Doom config in ~/Config
   home.file.".doom.d".source =
-    config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/Config/doom/.config/doom";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Config/doom/.config/doom";
 }
